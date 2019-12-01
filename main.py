dac3a4c1fb73d4f2d88ee2398f6b70b11655fcc3
Concertg <sasha.noskov.2227@mail.ru>
2019-12-01 11:28:38 +1000

    -

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ login = "Concertg"
 email = "sasha.noskov.2227@mail.ru"
 repository = "My1"
 print('-' * 70)
-
 password = input("Ведите пароль: ")
 print("\033[A" + 'Ведите пароль: ', '*' * len(password))
 
@@ -16,7 +15,7 @@ if os.name == "nt":
 else:
     path = "/home/algoritmika/Документы"
 
-url = "https://api.github.com/"
+url = "https://api.github.com/user/repos"
 auth = (login, password)
 data = {
     "name": f"{repository}",
@@ -27,13 +26,14 @@ if repos.status_code == 200:
 else:
     print("[ERROR] вход не вполнен")
 new_repository = rs.post(url, auth=auth, data=js.dumps(data))
+for repo in repos.json():
+    print(repo["html_url"])
 os.chdir(path)
 _, folder = os.path.split(os.getcwd())
 os.system(f"git clone https://github.com/{login}/{repository}.git")
-
 os.chdir(f'{path}/{repository}')
 os.system(f'git config user.email "{email}" && git config user.name "{login}" && git remote set-url origin https://{login}:{password}@github.com/{login}/{repository}.git')
-f = open('main.py','a')
+f = open('main.py', 'a')
 f.close()
 print(f'[INFO] Выход\n{"-" * 70}')
 tm.sleep(2)
